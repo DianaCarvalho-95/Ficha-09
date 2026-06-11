4549c309f8b084e8f6abede016275bb0f25f3817
Diana Carvalho <1232099@isep.ipp.pt>
2026-06-11 18:31:13 +0100

    Carregar utilizadores

--- config/config.php
+++ config/config.php
@@ -6,3 +6,10 @@ define('BASE_URL', '/Ficha%2009');
 define('APP_NAME', 'ISEP Ginásio');
 define('APP_VERSION', '1.0.0');
 define('APP_COPYRIGHT', '© 2025 ISEP');
+
+// Configurações da Base de Dados
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', '3306');
+define('DB_NAME', 'isep-ginasio');
+define('DB_USER', 'root');
+define('DB_PASS', '');
